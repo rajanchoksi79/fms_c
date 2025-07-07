79ecd8e02fc38b298d417503c148b76477399dd6
Rajan Choksi <rachoksi9999@gmail.com>
2025-07-07 11:28:06 +0530

    added letter f to all flags that represent for file operations

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
     char *path_one = argv[2];
     char *text, *path_two;
 
-    if (strcmp(flag, "--write") == 0)
+    if (strcmp(flag, "--writef") == 0)
     {
         if (argv[3] == NULL)
         {
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
         text = argv[3];
     }
 
-    if (strcmp(flag, "--copy") == 0 || strcmp(flag, "--move") == 0 || strcmp(flag, "--rename") == 0)
+    if (strcmp(flag, "--copyf") == 0 || strcmp(flag, "--movef") == 0 || strcmp(flag, "--renamef") == 0)
     {
         if (argv[3] == NULL)
         {
@@ -32,35 +32,35 @@ int main(int argc, char *argv[])
         path_two = argv[3];
     }
 
-    if (strcmp(flag, "--create") == 0)
+    if (strcmp(flag, "--createf") == 0)
     {
         create_file(path_one);
     }
-    else if (strcmp(flag, "--read") == 0)
+    else if (strcmp(flag, "--readf") == 0)
     {
         read_file(path_one);
     }
-    else if (strcmp(flag,"--write") == 0)
+    else if (strcmp(flag,"--writef") == 0)
     {
         write_file(path_one, text);
     }
-    else if (strcmp(flag, "--copy") == 0)
+    else if (strcmp(flag, "--copyf") == 0)
     {
         copy_file(path_one, path_two);
     }
-    else if (strcmp(flag, "--delete") == 0)
+    else if (strcmp(flag, "--deletef") == 0)
     {
         delete_file(path_one);
     }
-    else if (strcmp(flag, "--rename") == 0)
+    else if (strcmp(flag, "--renamef") == 0)
     {
         rename_file(path_one, path_two);
     }
-    else if (strcmp(flag, "--move") == 0)
+    else if (strcmp(flag, "--movef") == 0)
     {
         move_file(path_one, path_two);
     }
-    else if (strcmp(flag, "--detail") == 0)
+    else if (strcmp(flag, "--detailf") == 0)
     {
         states_file(path_one);
     }
