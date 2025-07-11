36a9f1db2f8072c5644daa1245948e5a356cda1a
Rajan Choksi <rachoksi9999@gmail.com>
2025-07-11 14:24:40 +0530

    added switch case in d_type of dirent while is not working, so for now commented that

--- dir_op_one.c
+++ dir_op_one.c
@@ -6,17 +6,16 @@
 #include <string.h>
 #include <errno.h>
 
-
 int create_directory(char *path)
 {
-    if (access(path, F_OK) == 0) 
-    {   
+    if (access(path, F_OK) == 0)
+    {
         printf("Error occured, %s\n", strerror(errno));
         return 1;
     }
 
     int new_dir = mkdir(path, 0755);
-    if (new_dir == -1) 
+    if (new_dir == -1)
     {
         printf("Error occured, %s\n", strerror(errno));
         return 1;
@@ -27,19 +26,19 @@ int create_directory(char *path)
 }
 
 // reading and printing content of directory
-int read_directory(char *path) 
+int read_directory(char *path)
 {
-    if (access(path, F_OK) == -1) 
-    {   
+    if (access(path, F_OK) == -1)
+    {
         printf("Error occured, %s\n", strerror(errno));
         return 1;
     }
 
     DIR *dir = opendir(path);
-    if (dir == NULL) 
+    if (dir == NULL)
     {
         printf("Error occured, %s\n", strerror(errno));
-        if (closedir(dir) == -1) 
+        if (closedir(dir) == -1)
         {
             printf("Error occured, %s\n", strerror(errno));
             return 1;
@@ -48,16 +47,34 @@ int read_directory(char *path)
     }
 
     struct dirent *entry;
-    while ((entry = readdir(dir)) != NULL) 
+    while ((entry = readdir(dir)) != NULL)
     {
-        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) 
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
         {
             continue;
         }
+        
+        // remove \n from below when you use that switch case below line below
         printf("%s\n", entry->d_name);
+
+        // switch (entry->d_type)
+        // {
+        // case DT_REG:
+        //     printf(" [File]\n");
+        //     break;
+        // case DT_DIR:
+        //     printf(" [Directory]\n");
+        //     break;
+        // case DT_LINK:
+        //     printf(" [Link]\n");
+        //     break;
+        // default:
+        //     printf(" [Other]");
+        //     break;
+        // }
     }
 
-    if (closedir(dir) == -1) 
+    if (closedir(dir) == -1)
     {
         printf("Error occured, %s\n", strerror(errno));
         return 1;
@@ -66,15 +83,15 @@ int read_directory(char *path)
 }
 
 // this works only on empty directory.
-int remove_directory(char *path) 
+int remove_directory(char *path)
 {
-    if (access(path, F_OK) == -1) 
+    if (access(path, F_OK) == -1)
     {
         printf("Error occured, %s\n", strerror(errno));
         return 1;
     }
 
-    if(rmdir(path) == -1) 
+    if (rmdir(path) == -1)
     {
         printf("Error occured, %s\n", strerror(errno));
         return 1;
