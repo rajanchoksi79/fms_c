4f5d4f1812f05d1fae8b2f0ea1658c8d634be31f
Rajan Choksi <rachoksi9999@gmail.com>
2025-07-15 13:51:16 +0530

    handled argv and also structurize code in main

--- src/main.c
+++ src/main.c
@@ -14,115 +14,126 @@
 
 int main(int argc, char *argv[])
 {
-    
+
+    char *flag, *path_one, *path_two, *text;
+    mode_t permission_code;
+
     if (argc < 2) 
     {
         help_user();
     }
-
-    char *flag = argv[1];
-    char *path_one = argv[2];
-    char *text, *path_two;
-    mode_t permission_code;
-
-    if (strcmp(flag, "--writef") == 0)
+    else if (argc < 3) 
     {
-        if (argv[3] == NULL)
-        {
-            printf("Error occured, %s\n", strerror(errno));
-            return 1;
-        }
-        text = argv[3];
-    }
+        flag = argv[1];
 
-    if (strcmp(flag, "--copyf") == 0 || strcmp(flag, "--movef") == 0 || strcmp(flag, "--renamef") == 0)
+        if (strcmp(flag, "--currentdir") == 0) 
+        {
+            get_current_directory();
+        }
+        else if (strcmp(flag, "--help") == 0) 
+        {
+            help_user(); 
+        }
+        else
+        {
+            printf("please provide appropriate flag\n");
+            // add flag details to display to user.
+        }
+    }
+    else if (argc < 4) 
     {
-        if (argv[3] == NULL)
-        {
-            printf("Error occured, %s\n", strerror(errno));
-            return 1;
-        }
-        path_two = argv[3];
-    }
+        flag = argv[1];
+        path_one = argv[2];
 
-    // refactore and fix this, this is temp.
-    if (strcmp(flag, "--changeperf") == 0) 
-    {   
-        if (argv[3] == NULL) 
+        if (strcmp(flag, "--createf") == 0)
         {
-            printf("Error occured, %s\n", strerror(errno));
-            return 1;
+            create_file(path_one);
         }
-        permission_code = parse_octal_mode(argv[3]);
-        printf("permission code in main: %o\n", permission_code);
-        if (permission_code == (mode_t)-1) 
+        else if (strcmp(flag, "--readf") == 0)
+        {
+            read_file(path_one);
+        }
+        else if (strcmp(flag, "--removef") == 0)
+        {
+            remove_file(path_one);
+        }
+        else if (strcmp(flag, "--detailf") == 0)
+        {
+            states_file(path_one);
+        }
+        else if (strcmp(flag, "--createdir") == 0) 
+        {
+            create_directory(path_one);
+        }
+        else if (strcmp(flag, "--readdir") == 0) 
+        {
+            read_directory(path_one);
+        }
+        else if (strcmp(flag, "--removedir") == 0) 
+        {
+            remove_directory(path_one);
+        }
+        else
+        {
+            printf("please provide appropriate flag\n");
+            // add flag details to display to user.
+        }
+    }
+    else if (argc < 5) 
+    {
+        flag = argv[1];
+        path_one = argv[2];
+
+        if (strcmp(flag, "--writef") == 0)
+        {
+            if (argv[3] == NULL)
+            {
+                printf("Error occured, %s\n", strerror(errno));
+                return 1;
+            }
+            text = argv[3];
+            write_file(path_one, text);
+        }
+        else if (strcmp(flag, "--copyf") == 0 || strcmp(flag, "--movef") == 0 || strcmp(flag, "--renamef") == 0)
+        {
+            if (argv[3] == NULL)
+            {
+                printf("Error occured, %s\n", strerror(errno));
+                return 1;
+            }
+            path_two = argv[3];
+
+            if (strcmp(flag, "--copyf") == 0)
+            {
+                copy_file(path_one, path_two);
+            }
+            else if (strcmp(flag, "--movef") == 0)
+            {
+                move_file(path_one, path_two);
+            }
+            else if (strcmp(flag, "--renamef") == 0)
+            {
+                rename_file(path_one, path_two);
+            }
+        }
+        // refactore and fix this, this is temp.
+        else if (strcmp(flag, "--changeperf") == 0) 
         {   
-            // i already handled error in parse octal function, here i am just returning in case of error
-            return 1;
-        }
-    }
+            if (argv[3] == NULL) 
+            {
+                printf("Error occured, %s\n", strerror(errno));
+                return 1;
+            }
+            permission_code = parse_octal_mode(argv[3]);
+            if (permission_code == (mode_t)-1) 
+            {   
+                // i already handled error in parse octal function, here i am just returning in case of error
+                return 1;
+            }
 
-    if (strcmp(flag, "--createf") == 0)
-    {
-        create_file(path_one);
-    }
-    else if (strcmp(flag, "--readf") == 0)
-    {
-        read_file(path_one);
-    }
-    else if (strcmp(flag,"--writef") == 0)
-    {
-        write_file(path_one, text);
-    }
-    else if (strcmp(flag, "--copyf") == 0)
-    {
-        copy_file(path_one, path_two);
-    }
-    else if (strcmp(flag, "--removef") == 0)
-    {
-        remove_file(path_one);
-    }
-    else if (strcmp(flag, "--renamef") == 0)
-    {
-        rename_file(path_one, path_two);
-    }
-    else if (strcmp(flag, "--movef") == 0)
-    {
-        move_file(path_one, path_two);
-    }
-    else if (strcmp(flag, "--detailf") == 0)
-    {
-        states_file(path_one);
-    }
-    else if (strcmp(flag, "--changeperf") == 0) 
-    {
-        change_file_permission(path_one, permission_code);
-    }
-    else if (strcmp(flag, "--help") == 0) 
-    {
-        help_user(); 
-    }
-    else if (strcmp(flag, "--createdir") == 0) 
-    {
-        create_directory(path_one);
-    }
-    else if (strcmp(flag, "--readdir") == 0) 
-    {
-        read_directory(path_one);
-    }
-    else if (strcmp(flag, "--currentdir") == 0) 
-    {
-        get_current_directory();
-    }
-    else if (strcmp(flag, "--removedir") == 0) 
-    {
-        remove_directory(path_one);
-    }
-    else
-    {
-        printf("please provide appropriate flag\n");
-        // add flag details to display to user.
-    }
+            change_file_permission(path_one, permission_code);
+        }
+    }    
 
     return 0;
 }
