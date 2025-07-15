8c499d756b956a7bd0fb6fc0078af7e1a76f5e26
Rajan Choksi <rachoksi9999@gmail.com>
2025-07-15 14:41:26 +0530

    add formatting in appropriate flag function to display details better

--- src/mis_op.one.c
+++ src/mis_op.one.c
@@ -21,33 +21,37 @@ int about()
 
 int appropriate_flag() 
 {
+    printf("\n");
     printf("please provide appropriate flag\n");
     printf("\n");
     
     printf("File operation related flags\n");
     printf("\n");
-    printf("--createf:\tfor creating new file\n");
-    printf("--readf:\tfor reading file\n");
-    printf("--writef:\tfor writing in a file\n");
-    printf("--removef:\tfor removing file\n");
-    printf("--detailf:\tfor knowing details of a file\n");
-    printf("--copyf:\tfor copying a file\n");
-    printf("--movef:\tfor moving a file\n");
-    printf("--renamef:\tfor renaming a file\n");
-    printf("--changeperf:\tfor changing permissions in a file\n");
-    
+    printf("%-20s %s\n", "--createf", "for creating new file");
+    printf("%-20s %s\n", "--readf", "for reading file");
+    printf("%-20s %s\n", "--writef", "for writing in a file");
+    printf("%-20s %s\n", "--removef", "for removing file");
+    printf("%-20s %s\n", "--detailf", "for knowing details of a file");
+    printf("%-20s %s\n", "--copyf", "for copying a file");
+    printf("%-20s %s\n", "--movef", "for moving a file");
+    printf("%-20s %s\n", "--renamef", "for renaming a file");
+    printf("%-20s %s\n", "--changeperf", "for changing permissions in a file");
+    printf("\n");
+
     printf("Directory operation related flags\n");
     printf("\n");
-    printf("--createdir:\tfor creating new directory\n");
-    printf("--readdir:\tfor reading contents of a directory\n");
-    printf("--removedir:\tfor removing an empty directory\n");
-    printf("--currentdir:\tfor getting current working directory\n");
-    
+    printf("%-20s %s\n", "--createdir", "for creating new directory");
+    printf("%-20s %s\n", "--readdir", "for reading contents of a directory");
+    printf("%-20s %s\n", "--removedir", "for removing an empty directory");
+    printf("%-20s %s\n", "--currentdir", "for getting current working directory");
+    printf("\n");
+
     printf("Other flags\n");
     printf("\n");
-    printf("--help:\tfor help to use this tool");
-    printf("--about:\tfor knowing more about this tool");
-    printf("--version:\tfor getting version of this tool");
+    printf("%-20s %s\n", "--help", "for help to use this tool");
+    printf("%-20s %s\n", "--about", "for knowing more about this tool");
+    printf("%-20s %s\n", "--version", "for getting version of this tool");
+    printf("\n");
     
     return 0;
 }
