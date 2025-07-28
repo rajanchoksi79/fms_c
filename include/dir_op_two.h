6902b1eee0c75fd813a61e0cc04a99cc8217ff92
Rajan Choksi <rachoksi9999@gmail.com>
2025-07-28 14:12:39 +0530

    header of get_path function added in header file

--- include/dir_op_two.h
+++ include/dir_op_two.h
@@ -2,6 +2,7 @@
 #define dir_op_two
 
 int move_directory(char *rel_path);
+const char *get_basename(const char *rel_path);
 int print_directory_content(const char *rel_path, const struct stat *stat_buf, int typeflag, struct FTW *ftw_buf);
 int read_directory_rec(char *path);
 
